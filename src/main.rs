7dc45468d5fd97725d0d3dfb5e3b05c59aa5ead8
Andrea del Rio <adelrio@gmail.com>
2018-05-30 11:43:27 -0700

    add a socket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
+extern crate nix;
+
+use nix::sys::socket::{socket, AddressFamily, SockFlag, SockType};
+
+fn main() -> Result<(), Box<std::error::Error>> {
+    let sock = socket(AddressFamily::Inet, SockType::Stream, SockFlag::empty(), None)?;
     println!("Hello, world!");
+    Ok(())
 }
